642fa0df152fff842b3d13fc8a91996192766887
LAPTOP-6BUUF8U8\MY LE <dammaynholuyenthuyen@gmail.com>
2025-04-05 21:57:07 +0700

    create component

---
+++ src/page/register.tsx
@@ -0,0 +1,17 @@
+function Home() {
+    return (
+      <>
+      <h1>Register</h1>
+      <div className="p-4">
+        <button className="button1">hihi</button>
+        <button className="button2">hoho</button>
+        <button className="button3">hehe</button>
+        <button className="state1">huhu</button>
+        <button className="state2">hihi</button>
+        <button className="state3">hoho</button>
+      </div>
+      </>
+      );
+}
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Roboto: ['Roboto', 'sans-serif']
+      },
+      colors: {
+        dark_blue: "#2563EB",
+        light_blue:"#EEF4FE",
+        dark_pink:"#ED4F9D",
+        light_pink:"#FDEDF5",
+        dark_green:"#24D164",
+        light_green:"#E9FAEF",
+        light_red: "rgba(237, 79, 82, 0.851)",
+        black_admin:"#23272C"
+      },
+    },
   },
   plugins: [],
 }
