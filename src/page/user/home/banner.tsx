6b5cee368b25af9f075e1f7665c073d9951e030b
hieuduongk22bk <hieu.duongk22bk@hcmut.edu.vn>
2025-04-09 01:00:46 +0700

    hieu

--- src/page/user/home/banner.tsx
+++ src/page/user/home/banner.tsx
@@ -4,27 +4,32 @@ import { IoIosSearch } from "react-icons/io";
 function Banner() {
   return (
     <>
-      <div className="relative aspect-[16/9] w-full">
+      <div className="relative aspect-[1563/580] w-full">
         <img src={BannerImg} className="object-cover" />
 
-        <div className="absolute bottom-4 left-0 top-1/2 w-full px-4">
+        <div className="absolute left-0 top-[73%] w-full px-4">
           <div className="max-w-7xl w-full mx-auto flex flex-col md:flex-row gap-4">
             <div className="md:basis-1/2 w-full">
               <button className="button1 w-full md:w-auto">
                 Đặt phòng ngay!
               </button>
             </div>
+
             <div className="flex flex-row md:basis-1/2 w-full">
-              <form className="w-full">
+              <div className="flex items-center w-full relative">
                 <input
                   type="text"
-                  placeholder="Tìm kiếm phòng học"
-                  className="w-full p-2 border rounded"
+                  placeholder="Tìm kiếm phòng học..."
+                  className="w-full p-2 pr-10 border rounded"
                 />
-              </form>
-
-              <div className="absolute left-[90%] justify-center">
-                <IoIosSearch />
+                <div className="absolute right-8 flex items-center">
+                  <button
+                    type="submit"
+                    className="absolute flex items-center text-gray-600 hover:text-black"
+                  >
+                    <IoIosSearch size={20} />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
