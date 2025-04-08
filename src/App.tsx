408f8f5e26c8d84d315dba207373d3e5e0b8a472
hieuduongk22bk <hieu.duongk22bk@hcmut.edu.vn>
2025-04-08 19:37:52 +0700

    shorten tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Book from "./page/user/book";
 import History from "./page/user/history";
 import Register from "./page/user/register";
-import Home from "./page/user/home";
+import Home from "./page/user/home/home";
 import Login from "./page/user/login";
 import StatusRoom from "./page/user/status";
 import ReportIssue from "./page/user/report";
@@ -12,10 +12,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Home />} />
         <Route path="/book" element={<Book />} />
         <Route path="/history" element={<History />} />
-        <Route path="/register" element={<Home />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/status" element={<StatusRoom />} />
         <Route path="/report" element={<ReportIssue />} />
