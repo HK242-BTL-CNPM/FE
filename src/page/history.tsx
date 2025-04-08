95e021cf7a11bf63d9d1512d57c93a41a4419a3a
HTH\Hai <hathienhai4@gmail.com>
2025-04-08 21:19:22 +0700

    .

--- src/page/history.tsx
+++
@@ -1,9 +0,0 @@
-function History() {
-    return (
-        <div className="text-red-500 ">
-            HISTORY
-        </div>
-      );
-}
-
-export default History;
--- src/page/status.tsx
+++
@@ -1,5 +0,0 @@
-function Status() {
-  return <div className="text-red-500 ">STATUS</div>;
-}
-
-export default Status;
