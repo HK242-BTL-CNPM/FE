33602a2d334cb2173b58024f10dc5247f8c8de13
HTH\Hai <hathienhai4@gmail.com>
2025-04-08 21:20:57 +0700

    Revert "."
    This reverts commit 95e021cf7a11bf63d9d1512d57c93a41a4419a3a.

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Book from "./page/book";
+import History from "./page/history";
+import Register from "./page/register";
+import Home from "./page/home";
+import Login from "./page/login";
+import StatusRoom from "./page/status";
+import ReportIssue from "./page/report";
+import "./assets/css/output.css";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/register" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/status" element={<StatusRoom />} />
+        <Route path="/report" element={<ReportIssue />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/page/history.tsx
@@ -0,0 +1,9 @@
+function History() {
+    return (
+        <div className="text-red-500 ">
+            HISTORY
+        </div>
+      );
+}
+
+export default History;
