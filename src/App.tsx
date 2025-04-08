7c100cf85f3c7b86362dc3cfc14780c3f2d481d8
MileeDevWork <letrinh0904.work@gmail.com>
2025-04-08 15:33:05 +0700

    tổ chức lại file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import Book from "./page/book";
-import History from "./page/history";
-import Register from "./page/register";
-import Home from "./page/home";
-import Login from "./page/login";
-import StatusRoom from "./page/status";
-import ReportIssue from "./page/report";
+import Book from "./page/user/book";
+import History from "./page/user/history";
+import Register from "./page/user/register";
+import Home from "./page/user/home";
+import Login from "./page/user/login";
+import StatusRoom from "./page/user/status";
+import ReportIssue from "./page/user/report";
 import "./assets/css/output.css";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Register />} />
         <Route path="/book" element={<Book />} />
         <Route path="/history" element={<History />} />
         <Route path="/register" element={<Home />} />
--- src/page/user/register.tsx
+++ src/page/user/register.tsx
@@ -5,7 +5,7 @@ function Home() {
       <div className="p-4">
         <button className="button1">hihi</button>
         <button className="button2">hoho</button>
-        <button className="button3">hehe</button>
+        {/* <button className="button3">hehe</button> */}
         <button className="state1">huhu</button>
         <button className="state2">hihi</button>
         <button className="state3">hoho</button>
