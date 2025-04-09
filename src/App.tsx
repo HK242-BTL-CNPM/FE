a251e169a66afb3b6f8d592dc062fc04fb42faf8
LAPTOP-6BUUF8U8\MY LE <dammaynholuyenthuyen@gmail.com>
2025-04-09 15:43:38 +0700

    add sidebbar admin

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import Home from "./page/user/home/home";
 import Login from "./page/auth/login";
 import StatusRoom from "./page/user/status/status";
 import ReportIssue from "./page/user/report/report";
+import Dashboard from "./page/admin/Dashboard";
 import "./assets/css/output.css";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/book" element={<Book />} />
         <Route path="/history" element={<History />} />
         <Route path="/login" element={<Login />} />
         <Route path="/status" element={<StatusRoom />} />
         <Route path="/report" element={<ReportIssue />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </>
   );
