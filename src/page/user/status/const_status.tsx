ab951b6f5bacccc37cda78297514fcdcfb1b744c
HTH\Hai <hathienhai4@gmail.com>
2025-04-09 21:28:57 +0700

    Update

--- src/page/user/status/const_status.tsx
+++ src/page/user/status/const_status.tsx
@@ -1,34 +1,46 @@
 export const rooms = [
-    { id: 'A01', type: 'Phòng cá nhân', status: 'ĐÃ ĐẶT', time: '11:00 - 12:00' },
-    { id: 'B01', type: 'Phòng mentor 1 - 1', status: 'TRỐNG', time: '-- : --' },
-    { id: 'C01', type: 'Phòng học nhóm', status: 'TRỐNG', time: '-- : --' },
-    { id: 'D01', type: 'Phòng thuyết trình', status: 'KHÓA', time: '-- : --' },
-    { id: 'A02', type: 'Phòng cá nhân', status: 'ĐANG SỬ DỤNG', time: '7:00 - 9:00' },
-    { id: 'A03', type: 'Phòng cá nhân', status: 'ĐANG SỬ DỤNG', time: '7:00 - 9:00' },
-    { id: 'B02', type: 'Phòng mentor 1 - 1', status: 'ĐANG SỬ DỤNG', time: '7:00 - 9:00' },
-    { id: 'C02', type: 'Phòng học nhóm', status: 'ĐÃ ĐẶT', time: '7:00 - 9:00' }
-  ];
-  export const roomStatuses = [
-    "Tất cả",
-    "Trống",
-    "Khóa",
-    "Đã đặt",
-    "Đang sử dụng",
-  ];
-  export const statusColor = {
-    'TRỐNG': 'bg-green-200 text-green-800',
-    'KHÓA': 'bg-purple-200 text-purple-800',
-    'ĐÃ ĐẶT': 'bg-yellow-200 text-yellow-800',
-    'ĐANG SỬ DỤNG': 'bg-sky-200 text-sky-800'
-  };
-  
-  export const roomTypes = [
-    "Tất cả phòng",
-    "Phòng cá nhân",
-    "Phòng mentor 1-1",
-    "Phòng học nhóm",
-    "Phòng thuyết trình",
-  ];
-  
+  { id: "A01", type: "Phòng cá nhân", status: "Đã đặt", time: "11:00 - 12:00" },
+  { id: "B01", type: "Phòng mentor 1 - 1", status: "Trống", time: "-- : --" },
+  { id: "C01", type: "Phòng học nhóm", status: "Trống", time: "-- : --" },
+  { id: "D01", type: "Phòng thuyết trình", status: "Khóa", time: "-- : --" },
+  {
+    id: "A02",
+    type: "Phòng cá nhân",
+    status: "Đang sử dụng",
+    time: "7:00 - 9:00",
+  },
+  {
+    id: "A03",
+    type: "Phòng cá nhân",
+    status: "Đang sử dụng",
+    time: "7:00 - 9:00",
+  },
+  {
+    id: "B02",
+    type: "Phòng mentor 1 - 1",
+    status: "Đang sử dụng",
+    time: "7:00 - 9:00",
+  },
+  { id: "C02", type: "Phòng học nhóm", status: "Đã đặt", time: "7:00 - 9:00" },
+];
+export const roomStatuses = [
+  "Tất cả",
+  "Trống",
+  "Khóa",
+  "Đã đặt",
+  "Đang sử dụng",
+];
+export const statusColor = {
+  Trống: "status-green",
+  Khóa: "status-purple",
+  "Đã đặt": "status-yellow",
+  "Đang sử dụng": "status-sky",
+};
 
-  
+export const roomTypes = [
+  "Tất cả phòng",
+  "Phòng cá nhân",
+  "Phòng mentor 1-1",
+  "Phòng học nhóm",
+  "Phòng thuyết trình",
+];
