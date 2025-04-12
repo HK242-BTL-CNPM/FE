0597d233ed05f22a06b2e99d30966a4551cd3270
mimin514 <my.nguyen2004@hcmut.edu.vn>
2025-04-13 00:12:35 +0700

    update thêm nút đóng mở sidebar: build ok

--- src/page/admin/components/header_admin.tsx
+++ src/page/admin/components/header_admin.tsx
@@ -1,9 +1,15 @@
 import AdminUserMenu from "../../admin/components/menu_admin"; // Import component mới
-import { BellIcon } from "@heroicons/react/24/outline";
+import { BellIcon,Bars3Icon} from "@heroicons/react/24/outline";
 
-function Header_admin() {
+function Header_admin({ onToggleSidebar }: { onToggleSidebar: () => void }) {
   return (
-    <div className="flex bg-white h-20 items-center justify-end px-4">
+    <div className="flex bg-white h-20 items-center justify-between px-4">
+      <button
+        onClick={onToggleSidebar}
+        className="text-gray-600 focus:outline-none "
+      >
+        <Bars3Icon className="h-8 w-8" />
+      </button>
       <div className="flex items-center pr-4">
         <button className="text-gray-600 focus:outline-none">
           <BellIcon className="h-8 w-8" />
@@ -11,6 +17,7 @@ function Header_admin() {
       </div>
       <AdminUserMenu />
     </div>
+
   );
 }
 
