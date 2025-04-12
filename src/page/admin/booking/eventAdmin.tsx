e58ec66b44fbcbeedf3e88b74b11507aa8bc4dc8
hieuduongk22bk <hieu.duongk22bk@hcmut.edu.vn>
2025-04-13 01:44:59 +0700

    i

---
+++ src/page/admin/booking/eventAdmin.tsx
@@ -0,0 +1,43 @@
+export const events = [
+  {
+    id: '1',
+    title: 'CS1-B1-B1-02',
+    start: '2025-04-16 10:00',
+      end: '2025-04-16 12:00',
+    description: 'Phòng cá nhân.',
+    
+  },
+  {
+    id: '2',
+    title: 'CS1-B3-B3-03',
+    start: '2025-04-17 08:00',
+    end: '2025-04-17 09:00',
+    description: 'Phòng cá nhân.',
+   calendarId: 'hieu'
+  },
+  {
+    id: '3',
+    title: 'CS2-H1-H1-04',
+    start: '2025-04-16 10:00',
+    end: '2025-04-16 12:00',
+    description: 'Phòng cá nhân.',
+    calendarId: 'leisure'
+  },
+  {
+    id: '4',
+    title: 'CS2-H1-H1-05',
+    start: '2025-04-18 10:00',
+    end: '2025-04-18 12:00',
+    description: 'Phòng cá nhân.',
+    calendarId: 'leisure'
+  },
+  {
+    id: '5',
+    title: 'CS2-H3-H3-06',
+    start: '2025-04-17 13:00',
+    end: '2025-04-17 15:00',
+    description: 'Phòng cá nhân.',
+    calendarId: 'leisure'
+  },
+
+];
