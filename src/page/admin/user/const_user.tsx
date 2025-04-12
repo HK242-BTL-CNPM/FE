00c8c440b46eb4de9e140953a486b55480943d52
mimin514 <my.nguyen2004@hcmut.edu.vn>
2025-04-13 01:51:15 +0700

    respponsive: ok

---
+++ src/page/admin/user/const_user.tsx
@@ -0,0 +1,58 @@
+export const users = [
+    {
+      id: 1,
+      fullName: "Hà Thiên Hải",
+      studentId: "2210882",
+      email: "hai.sadboisitik@gmail.com",
+      status: "Bị khóa",
+    },
+    {
+      id: 2,
+      fullName: "Dương Minh Hiếu",
+      studentId: "2210978",
+      email: "hieu.AnhHungGiaiCuuVuTru@gmail.com",
+      status: "Hoạt động",
+    },
+    {
+      id: 3,
+      fullName: "Lê Đăng Huy",
+      studentId: "2211186",
+      email: "huy_fullstack@gmail.com",
+      status: "Hoạt động",
+    },
+    {
+      id: 4,
+      fullName: "Trương Anh Khôi",
+      studentId: "2211701",
+      email: "khoi_acquy123@gmail.com",
+      status: "Bị khóa",
+    },
+    {
+      id: 5,
+      fullName: "Trịnh Thị Mỹ Lệ",
+      studentId: "2211832",
+      email: "le.xinhgaihocgioi@gmail.com",
+      status: "Hoạt động",
+    },
+    {
+      id: 6,
+      fullName: "Nguyễn Ngọc Hà My",
+      studentId: "2212104",
+      email: "mymeme@gmail.com",
+      status: "Hoạt động",
+    },
+    {
+      id: 7,
+      fullName: "Tăng Phồn Thịnh",
+      studentId: "2213307",
+      email: "thinh.ganhteam@gmail.com",
+      status: "Hoạt động",
+    },
+  ];
+  
+  export const userStatusColor = {
+    "Hoạt động": "state3",
+    "Bị khóa": "state1",
+    "status-default": "",
+  };
+  
